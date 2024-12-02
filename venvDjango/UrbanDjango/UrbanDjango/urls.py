685a7aabbf56aae470d0ccd0e59b84a3198ffa90
rootcrop <rootcrop@gmail.com>
2024-12-02 18:09:46 +0300

    second

--- venvDjango/UrbanDjango/UrbanDjango/urls.py
+++ venvDjango/UrbanDjango/UrbanDjango/urls.py
@@ -1,8 +1,6 @@
 """
-URL configuration for UrbanDjango project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+URL configuration for UrbanDjango project. The `urlpatterns` list routes URLs to views. For more information please see:
+https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +14,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from example1.views import index    #1 способ #главная страница index это функция, которую нужно импортировать
+from example1.views import index2   #2 способ #уже как класс
+from django.views.generic import TemplateView   #3 способ: из views импортировать TemplateView и создавать класс сразу в urlpatterns
+from task2.views import index11
+from task2.views import index22
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index11.as_view() ),   # '' пустое место означает по умолчанию, т.е. главная страница
+    path('2', index22 ),
+    path('3', index),               # index это функция, которую нужно импортировать
+    path('4', index2.as_view()),  # это уже не функция, а класс, поэтому нужен метод as_view
+    path('5', TemplateView.as_view(template_name='index3.html')),   # способ без views.py, с созданием объекта сразу тут
+
 ]
--- venvDjango/UrbanDjango/task2/views.py
+++ venvDjango/UrbanDjango/task2/views.py
@@ -8,17 +8,25 @@ from django.views.generic import TemplateView
 ### views и url представляют всю логику приложения
 ### views создаем функции обрабатывающие логику отображения и возвращающие шаблоны отрисовки для пользователя
 ### url отвечает за обработку пути запроса в браузере
+
 # 1 способ
-def index(request):                                       # это функция
-    return render(request, 'index.html')    # шаблон index.html берем в settings.py в TEMPLATES там список
+#def index11(request):                                       # это функция
+#    return render(request, 'index11.html')    # шаблон index.html берем в settings.py в TEMPLATES там список
 
 # делаем шаблон через объект
 # 2 способ
-class index2(TemplateView):   # наследуемся от базового шаблона в котором своя логика
-    template_name = 'index2.html'
+#class index22(TemplateView):   # наследуемся от базового шаблона в котором своя логика
+#    template_name = 'index22.html'
 
 # делаем шаблон через объект TemplateView сразу в urlpatterns
 # 3 способ
 # здесь ничего писать уже не нужно
 
+#def index11(request):   # функциональное представлениме
+#    return render(request, 'second_task/index11.html')    #
 
+class index11(TemplateView):   # объектное представлениме
+    template_name = 'second_task/index11.html'
+
+def index22(request):                                       #
+    return render(request, 'second_task/index22.html')    #
